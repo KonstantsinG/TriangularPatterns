86825abf49cada04c4976785c8d35b39b958c6a0
KonstantsinG <galajcukk@gmail.com>
2025-09-14 09:52:58 +0300

    Test ssh commit

--- scripts/greedy_triangulator.gd
+++ scripts/greedy_triangulator.gd
@@ -4,8 +4,7 @@ extends RefCounted
 
 func triangulate(points : Array[Vector2]) -> Array[Triangle]:
 	var triangles : Array[Triangle] = []
-	
-	var lala := UniformGrid.new([], 0.0, Vector2i.DOWN)
-	lala.get_neighbors()
+
+
 
 	return triangles
